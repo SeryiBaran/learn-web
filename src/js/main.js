bbc59b79f03e8e4128d9ce7b5b005a3463e3d318
SeryiBaran <seryibaranminepy@yandex.ru>
2022-03-05 15:56:35 +0300

    Init commit

---
+++ src/js/main.js
@@ -0,0 +1,100 @@
+// Импорты
+import "../scss/standard.scss";
+import "../scss/style.scss";
+
+// Счетчик времени
+let allTime = Date.now();
+
+// Тестовый стенд для строк
+console.log("\x1b[32m%s", "--------------------START--------------------");
+
+const textStart = "Привет, мир!";
+console.log(textStart);
+
+let textLetters = [];
+Array.from(textStart).forEach((elem) => {
+  textLetters.push(elem);
+});
+
+console.log(textLetters);
+textLetters.forEach((elem) => {
+  console.log(elem);
+});
+
+const textDone = textLetters.join("");
+console.log(textDone);
+
+console.log("\x1b[32m%s", "--------------------END--------------------");
+
+// Деструктуризация
+console.log("\x1b[32m%s", "--------------------START--------------------");
+
+function nestedArrayAndObject() {
+  const info = {
+    title: "Once Upon a Time",
+    protagonist: {
+      name: "Emma Swan",
+      enemies: [
+        { name: "Regina Mills", title: "Evil Queen" },
+        { name: "Cora Mills", title: "Queen of Hearts" },
+        { name: "Peter Pan", title: "The boy who wouldn't grow up" },
+        { name: "Zelena", title: "The Wicked Witch" },
+      ],
+    },
+  };
+  const {
+    title,
+    protagonist: { name: protagonistName },
+    protagonist: {
+      enemies: { 3: enemy },
+    },
+    enemyTitle = enemy.title,
+    enemyName = enemy.name,
+  } = info;
+  return `${enemyName} (${enemyTitle}) is an enemy to ${protagonistName} in "${title}"`;
+}
+
+console.log(nestedArrayAndObject());
+
+console.log("\x1b[32m%s", "--------------------END--------------------");
+
+// Параметры по умолчанию
+console.log("\x1b[32m%s", "--------------------START--------------------");
+
+function plusTest(a = 5, b = 5) {
+  return a + b;
+}
+
+console.log(plusTest());
+console.log(plusTest(10));
+console.log(plusTest(10, 10));
+
+console.log("\x1b[32m%s", "--------------------END--------------------");
+
+// Замер времени
+console.log("\x1b[32m%s", "--------------------START--------------------");
+
+let time = Date.now();
+
+let numberOfIterations = 30;
+let bits = 1;
+let bitsAll = [];
+
+for (let i = 0; i < numberOfIterations; i++) {
+  bits += bits;
+  bitsAll.push(bits);
+}
+
+console.log(bitsAll);
+
+time = Date.now() - time;
+console.log(`Время вычисления = ${time} миллисекунд`);
+
+console.log("\x1b[32m%s", "--------------------END--------------------");
+
+allTime = Date.now() - allTime;
+console.log(
+  "%c%s",
+  "color: green; font: 1.5rem Tahoma;",
+  `Время выполнения скрипта = ${allTime} миллисекунд`
+);
