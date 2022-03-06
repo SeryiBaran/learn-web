2fd2c22d3559a1a1a2fa40c33453206bad8c80ab
SeryiBaran <seryibaranminepy@yandex.ru>
2022-03-06 15:15:14 +0300

    Кнопка из инета

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,44 @@
 // Импорты
-import "../scss/standard.scss";
-import "../scss/style.scss";
+import '../scss/standard.scss';
+import '../scss/style.scss';
 
 // Счетчик времени
 let allTime = Date.now();
 
 // Тестовый стенд для строк
-console.log("\x1b[32m%s", "--------------------START--------------------");
+console.log('\x1b[32m%s', '--------------------START--------------------');
 
-const textStart = "Привет, мир!";
+const textStart = 'Привет, мир!';
 console.log(textStart);
 
 let textLetters = [];
-Array.from(textStart).forEach((elem) => {
+Array.from(textStart).forEach(elem => {
   textLetters.push(elem);
 });
 
 console.log(textLetters);
-textLetters.forEach((elem) => {
+textLetters.forEach(elem => {
   console.log(elem);
 });
 
-const textDone = textLetters.join("");
+const textDone = textLetters.join('');
 console.log(textDone);
 
-console.log("\x1b[32m%s", "--------------------END--------------------");
+console.log('\x1b[32m%s', '--------------------END--------------------');
 
 // Деструктуризация
-console.log("\x1b[32m%s", "--------------------START--------------------");
+console.log('\x1b[32m%s', '--------------------START--------------------');
 
 function nestedArrayAndObject() {
   const info = {
-    title: "Once Upon a Time",
+    title: 'Once Upon a Time',
     protagonist: {
-      name: "Emma Swan",
+      name: 'Emma Swan',
       enemies: [
-        { name: "Regina Mills", title: "Evil Queen" },
-        { name: "Cora Mills", title: "Queen of Hearts" },
-        { name: "Peter Pan", title: "The boy who wouldn't grow up" },
-        { name: "Zelena", title: "The Wicked Witch" },
+        { name: 'Regina Mills', title: 'Evil Queen' },
+        { name: 'Cora Mills', title: 'Queen of Hearts' },
+        { name: 'Peter Pan', title: "The boy who wouldn't grow up" },
+        { name: 'Zelena', title: 'The Wicked Witch' },
       ],
     },
   };
@@ -56,10 +56,10 @@ function nestedArrayAndObject() {
 
 console.log(nestedArrayAndObject());
 
-console.log("\x1b[32m%s", "--------------------END--------------------");
+console.log('\x1b[32m%s', '--------------------END--------------------');
 
 // Параметры по умолчанию
-console.log("\x1b[32m%s", "--------------------START--------------------");
+console.log('\x1b[32m%s', '--------------------START--------------------');
 
 function plusTest(a = 5, b = 5) {
   return a + b;
@@ -69,10 +69,10 @@ console.log(plusTest());
 console.log(plusTest(10));
 console.log(plusTest(10, 10));
 
-console.log("\x1b[32m%s", "--------------------END--------------------");
+console.log('\x1b[32m%s', '--------------------END--------------------');
 
 // Замер времени
-console.log("\x1b[32m%s", "--------------------START--------------------");
+console.log('\x1b[32m%s', '--------------------START--------------------');
 
 let time = Date.now();
 
@@ -90,11 +90,31 @@ console.log(bitsAll);
 time = Date.now() - time;
 console.log(`Время вычисления = ${time} миллисекунд`);
 
-console.log("\x1b[32m%s", "--------------------END--------------------");
+console.log('\x1b[32m%s', '--------------------END--------------------');
+
+// Авто-вставка контента в блоки
+console.log('\x1b[32m%s', '--------------------START--------------------');
+
+const testGalleryElements = document.querySelectorAll(
+  '.test-gallery-container div',
+);
+
+function fillContent(arr) {
+  if (!arr.length) return;
+  const [even, odd, ...rest] = arr;
+  even.innerHTML = '<p>ТЕСТОВЫЙ ТЕКСТ</p>';
+  odd.innerHTML =
+    '<img src="https://via.placeholder.com/300x200?text=ТЕСТОВАЯ%20КАРТИНКА">';
+  return fillContent(rest);
+}
+
+fillContent(testGalleryElements);
+
+console.log('\x1b[32m%s', '--------------------END--------------------');
 
 allTime = Date.now() - allTime;
 console.log(
-  "%c%s",
-  "color: green; font: 1.5rem Tahoma;",
-  `Время выполнения скрипта = ${allTime} миллисекунд`
+  '%c%s',
+  'color: green; font: 1.5rem Tahoma;',
+  `Время выполнения скрипта = ${allTime} миллисекунд`,
 );
