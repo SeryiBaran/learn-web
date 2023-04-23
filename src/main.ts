4fd99d3a796472c99d7d8daceec6b5d425de6aa0
SeryiBaran <seryibaranminepy@yandex.ru>
2023-04-23 17:17:52 +0300

    users && users search

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,121 @@
+import Fuse from 'fuse.js';
+import type { UserGeneratorResponse } from './types/userGeneratorResponse';
+import 'iconify-icon';
 import 'gardevoir';
 import './css/style.pcss';
+
+function assert(condition: any, msg?: string): asserts condition {
+  if (!condition)
+    throw new Error(msg);
+}
+
+(() => {
+  const hideButton = document.querySelector<HTMLButtonElement>('#hideAll');
+  const wrapperForHiding = document.querySelector<HTMLDivElement>('#wrapperForHiding');
+
+  const hidedFromLocalstorage = JSON.parse(localStorage.getItem('allIsHided') || 'false');
+  let hided = hidedFromLocalstorage;
+
+  function renderButtonText() {
+    assert(hideButton !== null);
+    hideButton.innerText = hided ? 'Показать все' : 'Скрыть все';
+  }
+
+  function syncWithLS() {
+    localStorage.setItem('allIsHided', String(hided));
+  }
+
+  function syncWithWrapper() {
+    assert(wrapperForHiding !== null);
+
+    if (hided)
+      wrapperForHiding.classList.add('hidden');
+
+    else
+      wrapperForHiding.classList.remove('hidden');
+  }
+
+  function toggleAllHide() {
+    assert(wrapperForHiding !== null);
+    hided = !hided;
+    syncWithLS();
+    syncWithWrapper();
+    renderButtonText();
+  }
+
+  assert(hideButton !== null);
+  hideButton.addEventListener('click', () => {
+    toggleAllHide();
+  });
+
+  syncWithWrapper();
+  renderButtonText();
+})();
+
+// TODO: Сделай функцию которая будет загружать список данных с какого нибудь апи, и выведи их как карточки. Потом добавь сортировки по полям с объекта, а потом поиск
+
+(async () => {
+  const cardsContainer = document.querySelector<HTMLDivElement>('#usersCardsContainer');
+  const searchInput = document.querySelector<HTMLInputElement>('#usersSearchInput');
+
+  const userGeneratorResponse: UserGeneratorResponse = await fetch('https://randomuser.me/api?results=30').then(res => res.json());
+  const users = userGeneratorResponse.results;
+
+  const fuse = new Fuse(users, {
+    keys: ['name.title', 'name.first', 'name.last'],
+    threshold: 0.4,
+  });
+
+  function renderUsersCards(users) {
+    const cardsHTML = users.map((user) => {
+      const contactsData = [
+        { icon: 'ri:user-fill', value: `@${user.login.username}` },
+        { icon: 'ri:mail-fill', value: user.email },
+        { icon: 'ri:phone-fill', value: user.phone },
+        { icon: 'ri:home-fill', value: `${user.location.country}, ${user.location.city}, ${user.location.street.name}, ${user.location.street.number}` },
+      ];
+
+      const contactsHTML = contactsData.map((item) => {
+        return `
+  <li><iconify-icon icon="${item.icon}" class="icon"></iconify-icon>: ${item.value}</li>
+        `;
+      }).join('');
+
+      return `
+  <div class="userCard">
+    <img
+      src="${user.picture.large}"
+      alt="User Avatar"
+      class="userAvatar"
+    />
+    <p class="userName">
+    <iconify-icon icon="flag:${user.nat.toLowerCase()}-4x3" class="icon"></iconify-icon>${user.name.title}. ${user.name.first} ${user.name.last}
+    </p>
+    <ul class="userContacts">
+      ${contactsHTML}
+    </ul>
+  </div>
+      `;
+    }).join('');
+
+    assert(cardsContainer !== null);
+    cardsContainer.innerHTML = cardsHTML;
+  }
+
+  assert(searchInput !== null);
+  searchInput.addEventListener('input', (evt) => {
+    const target = evt.target as HTMLInputElement; // работает
+    // const target = evt.target; // не работает
+
+    const searchResults = fuse.search(target.value);
+    console.log(target.value.length <= 0);
+    console.log(searchResults);
+    if (target.value.length <= 0) {
+      renderUsersCards(users);
+      return;
+    }
+    renderUsersCards(searchResults.map(item => item.item));
+  });
+
+  renderUsersCards(users);
+})();
--- src/utils/jqueryAnimations.ts
+++ src/utils/jqueryAnimations.ts
@@ -1,17 +1,16 @@
 export const SlideAnimations = {
-  slideUp: function (element: HTMLElement, duration = 500) {
-    return new Promise(function (resolve, reject) {
-      element.style.height = element.offsetHeight + 'px';
-      element.style.transitionProperty = `height, margin, padding`;
-      element.style.transitionDuration = duration + 'ms';
-      element.offsetHeight;
+  slideUp(element: HTMLElement, duration = 500) {
+    return new Promise((resolve) => {
+      element.style.height = `${element.offsetHeight}px`;
+      element.style.transitionProperty = 'height, margin, padding';
+      element.style.transitionDuration = `${duration}ms`;
       element.style.overflow = 'hidden';
       element.style.height = '0';
       element.style.paddingTop = '0';
       element.style.paddingBottom = '0';
       element.style.marginTop = '0';
       element.style.marginBottom = '0';
-      window.setTimeout(function () {
+      window.setTimeout(() => {
         element.style.display = 'none';
         element.style.removeProperty('height');
         element.style.removeProperty('padding-top');
@@ -26,27 +25,27 @@ export const SlideAnimations = {
     });
   },
   slideDown(element: HTMLElement, duration = 500) {
-    return new Promise(function () {
+    return new Promise(() => {
       element.style.removeProperty('display');
       let display = window.getComputedStyle(element).display;
-      if (display === 'none') display = 'block';
+      if (display === 'none')
+        display = 'block';
       element.style.display = display;
-      let height = element.offsetHeight;
+      const height = element.offsetHeight;
       element.style.overflow = 'hidden';
       element.style.height = '0';
       element.style.paddingTop = '0';
       element.style.paddingBottom = '0';
       element.style.marginTop = '0';
       element.style.marginBottom = '0';
-      element.offsetHeight;
-      element.style.transitionProperty = `height, margin, padding`;
-      element.style.transitionDuration = duration + 'ms';
-      element.style.height = height + 'px';
+      element.style.transitionProperty = 'height, margin, padding';
+      element.style.transitionDuration = `${duration}ms`;
+      element.style.height = `${height}px`;
       element.style.removeProperty('padding-top');
       element.style.removeProperty('padding-bottom');
       element.style.removeProperty('margin-top');
       element.style.removeProperty('margin-bottom');
-      window.setTimeout(function () {
+      window.setTimeout(() => {
         element.style.removeProperty('height');
         element.style.removeProperty('overflow');
         element.style.removeProperty('transition-duration');
@@ -55,10 +54,10 @@ export const SlideAnimations = {
     });
   },
   slideToggle(element: HTMLElement, duration = 500) {
-    if (window.getComputedStyle(element).display === 'none') {
+    if (window.getComputedStyle(element).display === 'none')
       return this.slideDown(element, duration);
-    } else {
+
+    else
       return this.slideUp(element, duration);
-    }
   },
 };
